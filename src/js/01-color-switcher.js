bc634c769725dc07d2c1b659f185e344a0945ab8
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-06-05 23:27:26 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  startButton: document.querySelector('[data-start]'),
+  stopButton: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+let timerId;
 
+refs.stopButton.setAttribute('disabled', 'true');
+
+refs.startButton.addEventListener('click', onStartButtonClick);
+refs.stopButton.addEventListener('click', onStorButtonClick);
+
+function onStartButtonClick() {
+  refs.startButton.setAttribute('disabled', 'true');
+  refs.stopButton.removeAttribute('disabled');
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function onStorButtonClick() {
+  refs.stopButton.setAttribute('disabled', 'true');
+  refs.startButton.removeAttribute('disabled');
+  clearTimeout(timerId);
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
