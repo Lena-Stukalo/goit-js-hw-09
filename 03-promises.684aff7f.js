55d657d797a2d7768881156ce312681f23127f09
Lena-Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2022-06-06 17:17:33 +0000

    Deploying to gh-pages from @ Lena-Stukalo/goit-js-hw-09@4cdcadec6cd3ab48b6678bae5d17507a290d0949 🚀

--- 03-promises.684aff7f.js
+++
@@ -1,2 +0,0 @@
-({delay:document.querySelector('[name="delay"]'),step:document.querySelector('[name="step"]'),amount:document.querySelector('[name="amount"]'),submit:document.querySelector("button"),form:document.querySelector("form")}).form.addEventListener("submit",(function(e){e.preventDefault()}));
-//# sourceMappingURL=03-promises.684aff7f.js.map
--- 03-promises.ba272f80.js
+++
@@ -1,2 +0,0 @@
-(document.querySelector('[name="delay"]'),document.querySelector('[name="step"]'),document.querySelector('[name="amount"]'),document.querySelector("button"),document.querySelector("form")).addEventListener("submit",(function(e){e.preventDefault()}));
-//# sourceMappingURL=03-promises.ba272f80.js.map
